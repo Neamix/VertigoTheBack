a65e42169d33bee0df84b742b3836d20e41158dd
NEAMIX <abdalrhmanhussin44@gmail.com>
2023-04-29 00:40:07 +0300

    Modify User Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password')->nullable();
-            $table->foreignId('role_id')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
