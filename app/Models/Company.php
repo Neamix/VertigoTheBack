16cdc5d62bfda88c4eac0c65fca2f4c4eb664ad4
NEAMIX <abdalrhmanhussin44@gmail.com>
2023-05-24 14:53:35 +0300

    Get payment status and send email on success

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Services\MailerService;
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Log;
@@ -10,13 +12,24 @@ use Laravel\Cashier\Events\WebhookReceived;
 
 class Company extends Model
 {
-    use HasFactory,Billable;
+    use HasFactory,Billable,MailerService;
 
     protected $guarded = [];
 
-    public function register(array $request)
+    /**
+     * Register new workspace company with its root user
+     * 
+     * Note: In case of failing in subscription the company and 
+     * root user that has been created will be deleted
+     * 
+     * @param array $request
+     * 
+     * @return array 
+    */
+
+    public function register(array $request) : array
     {
-        // Create the company
+        /*** Create new company */
         $company = self::create([
             'name'  => $request['name'],
             'email' => $request['email'],
@@ -27,25 +40,75 @@ class Company extends Model
             'active' => false
         ]);
 
-        // Create main root user
-        $rootUser = User::generateRootUser($request['user']);
+        /*** Generate root user */
+        $rootUser = User::generateRootUser($request['user'],$company->id);
         $token =  $rootUser->login($rootUser->email, $request['user']['password'])['token'];
 
-        // Payment intent
-        $company
-        ->newSubscription('bronze_plan',['price_1N43m6KV5l49k0XZkSKy9RHQ'])
-        ->trialDays(10)
-        ->quantity(5)
-        ->create($request['billing']['id'], [
-            'name' => $request['name'],
-            'email' => $request['email'],
-        ]);
+        /*** Try to subscripe */
+        try {
+            $subscripe = $company
+            ->newSubscription('bronze_plan',['price_1N43m6KV5l49k0XZkSKy9RHQ'])
+            ->quantity(5)
+            ->create($request['billing']['id'], [
+                'name'  => ($request['billing']['name'] == null) ? $request['billing']['email'] : $request['user']['name'] ,
+                'email' => ($request['billing']['email'] == null) ? $request['billing']['email'] : $request['user']['email'],
+                'phone' =>  $request['billing']['phone'] ?? null
+            ]);
 
-        return [
-            'company' => $company,
-            'user'    => $rootUser,
-            'token'   => $token
-        ];
+            // Send Success Email
+            $this->subscripedSuccess([
+                'name'      => $company->last_invoice->customer_name,
+                'to_email'  => $company->last_invoice->customer_email,
+                'subject'   => 'Welcome on board',
+                'seats'     => $request['seats'],
+                'invoiceID' => $company->last_invoice_id,
+                'company'   => $company->id,
+                'value_per_seat'      => 20,
+                'credit_pm_last_four' => $company->pm_last_four,
+                'inovice_number' => $company->invoice_number,
+                'credit_pm_type' => $company->pm_type
+            ]);
+            
+            return [
+                'company' => $company,
+                'user'    => $rootUser,
+                'token'   => $token,
+                'status'  => "Success"
+            ];
+
+        } catch (Exception $e) {
+            /*** Deleted comapany and user */
+            $rootUser->delete();
+            $company->delete();
+
+            return [
+                'status'  => "Failed",
+                'message' => $e->getMessage()
+            ];
+        }
+    }
+
+    // Invoices 
+    public function generateInvoice($invoiceID)
+    {
+        /*** Generate strip invoice */
+        return $this->downloadInvoice($invoiceID);
+    }
+
+    // Attributes
+    public function getLastInvoiceAttribute()
+    {
+        return $this->invoices()->last()->asStripeInvoice();
+    }
+
+    public function getLastInvoiceIdAttribute()
+    {
+        return $this->invoices()->last()->asStripeInvoice()->id;
+    }
+
+    public function getInvoiceNumbertAttribute()
+    {
+        return $this->invoices()->last()->asStripeInvoice()->number;
     }
 
     // Relations
--- app/Services/MailerService.php
+++ app/Services/MailerService.php
@@ -28,6 +28,22 @@ trait MailerService
         self::sendEmail($send);
     }
 
+    public function subscripedSuccess($data = [])
+    {
+        $send['to_email'] = $data['to_email'];
+        $send['name'] = $data['name'];
+        $send['subject'] = 'Vertigo | '.$data['subject'];
+        $send['view'] = 'emails.Invoices.success';
+        $send['value_per_seats'] = $data['value_per_seat'];
+        $send['seats'] = $data['seats'];
+        $send['inovice_number'] = $data['inovice_number'];
+        $send['credit_pm_last_four'] = $data['credit_pm_last_four'];
+        $send['credit_pm_type'] = $data['credit_pm_type'];
+        $send['company' ] = $data['company'];
+        $send['invoiceID' ] = $data['invoiceID'];
+        self::sendEmail($send);
+    }   
+
 
     static function sendEmail($data)
     {
