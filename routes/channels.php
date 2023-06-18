d590ff63e7df5ac4f20082c4df4268c7ab5f8567
Neamix <abdalrhmanhussin44@gmail.com>
2023-06-18 19:46:01 +0300

    add validation to companies presentation channels

--- routes/channels.php
+++ routes/channels.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Broadcast;
 
 /*
@@ -13,10 +14,10 @@ use Illuminate\Support\Facades\Broadcast;
 |
 */
 
-Broadcast::channel('private-my-channel', function () {
-    return true;
-});
+Broadcast::channel('company.{company_id}', function ($user,$company_id) {
+    if ( $user->active_company_id == $company_id ) {
+        return ['company_id' => $company_id];
+    }
 
-Broadcast::channel('my-channel', function () {
-    return true;
+    return false;
 });
