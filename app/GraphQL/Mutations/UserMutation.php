c4c8e9ccae033001b2d17a37fd40ea79a59f62b1
Neamix <abdalrhmanhussin44@gmail.com>
2023-06-20 00:46:03 +0300

    Switch Company

--- app/GraphQL/Mutations/UserMutation.php
+++ app/GraphQL/Mutations/UserMutation.php
@@ -23,26 +23,31 @@ final class UserMutation
         return $this->user->login($args['input']['email'],$args['input']['password']);
     }
 
+    // Forget Password
     public function forgetPassword($_,array $args)
     {
         return $this->user->forgetPassword($args['input']['email']);
     }
 
+    // Reset Password
     public function resetPassword($_,array $args)
     {
         return $this->user->resetPassword($args['input']['email'],$args['input']['otp'],$args['input']['verificationID'],$args['input']['password']);
-    }
+    }   
 
+    // Add New Member In WorkSpace
     public function inviteMember($_,array $args)
     {
-        return $this->user->inviteRequest($args['input']);
+        return Auth::user()->inviteRequest($args['input']);
     }
 
+    // Edit Profile
     public  function profileEdit($_,array $args)
     {
         return Auth::user()->updateProfile($args['input']);
     }
 
+    // Change Email
     public function changEmail($_,array $args)
     {
         return $this->request->changEmail($args['email']);
@@ -58,5 +63,11 @@ final class UserMutation
     public function acceptInvitation($_,array $args)
     {
         return $this->user->acceptInvitation($args['input']);
+    }
+
+    // Switch Company
+    public function switchCompany($_,array $args)
+    {
+        return Auth::user()->switchCompany($args);
     }
 }
